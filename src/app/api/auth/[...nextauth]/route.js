1f6e5e783ad60dc97bb819a4de33588512cd5bcc
mdsaidul44 <islam88758@gmail.com>
2024-10-27 19:33:15 +0600

    some

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth/react";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 
--- src/services/AuthProvider.jsx
+++ src/services/AuthProvider.jsx
@@ -1,6 +1,6 @@
 "use client"
 import React from 'react';
-import { SessionProvider } from "next-auth/next";
+import { SessionProvider } from "next-auth/react";
 
 
 const AuthProvider = ({children}) => {
